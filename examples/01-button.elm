611f7b5bbec65b09cd31acf591d47d24a43acd4b
Garett Arrowood <garettarrowood@gmail.com>
2016-11-13 12:39:40 -0500

    Add Reset button to ex1

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -2,8 +2,6 @@ import Html exposing (Html, button, div, text)
 import Html.App as App
 import Html.Events exposing (onClick)
 
-
-
 main =
   App.beginnerProgram
     { model = model
@@ -12,40 +10,34 @@ main =
     }
 
 
-
 -- MODEL
 
-
 type alias Model = Int
-
 
 model : Model
 model =
   0
 
 
-
 -- UPDATE
-
 
 type Msg
   = Increment
   | Decrement
-
+  | Reset
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
       model + 1
-
     Decrement ->
       model - 1
-
+    Reset ->
+      0
 
 
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
@@ -53,4 +45,6 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , div [] []
+    , button [ onClick Reset ] [ text "Reset" ]
     ]
